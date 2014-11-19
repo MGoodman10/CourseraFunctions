4fee0249bd72303c867623b7016434a77993217a
Michael Goodman <msg.goodman@gmail.com>
2014-11-19 15:56:13 -0600

    Working rankhospital function

--- rankhospital.R
+++ rankhospital.R
@@ -1,4 +1,4 @@
-best <- function(state, outcome) {
+rankhospital <- function(state, outcome, num) {
         path <- paste(getwd(),"/", "hospdata", sep="")
         directory <- file.path(path, "outcome-of-care-measures.csv")
         raw.data <- read.csv(directory, header=TRUE)
@@ -7,7 +7,7 @@ best <- function(state, outcome) {
         outcome.idx <- c('Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack',
                          'Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure',
                          'Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia')
-        ##outcome.idx <- c('from.Heart.Attack',
+        ## outcome.idx <- c('from.Heart.Attack',
         ##                 'from.Heart.Failure',
         ##                 'from.Pneumonia')
         outcome.df <- data.frame(outcome.lst, outcome.idx)
@@ -21,8 +21,8 @@ best <- function(state, outcome) {
         names(subset.df) <- c("hosp.name","state.col", "mortality")
         state.data <- subset(subset.df, state.col==state)
         
-        num <- nrow(state.data)
-        if(num==0) { bst.hospital <- NULL
+        num.state <- nrow(state.data)
+        if(num.state==0) { bst.hospital <- NULL
                       return(bst.hospital)}
                 
         state.data$mortality[state.data$mortality=="Not Available"] <- NA
@@ -32,8 +32,15 @@ best <- function(state, outcome) {
                 
         order.pop <- order(good.data$mortality, good.data$hosp.name)
         result.data <- good.data[order.pop, ]
+        
+        if(num=='best') {hosp.rank <- 1}
+        else if(num=='worst') {hosp.rank <- nrow(good.data)}
+        else {hosp.rank <- num}
+        
+        if(hosp.rank > nrow(good.data)) { bst.hospital <- NA
+                           return(bst.hospital)}
                 
-        bst.hospital <- as.character(result.data$hosp.name[1])
+        bst.hospital <- as.character(result.data$hosp.name[hosp.rank])
         bst.hospital
         
                 
